dc9dd8e82051192adc2d012b5b9e05088c70ad61
Heidi Marasigan <iamheidi@192.168.254.100>
2025-02-24 17:19:04 +0800

    updated pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Restohub";
+export const SITE_DESCRIPTION = "Your restaurant supplies warehouse";
